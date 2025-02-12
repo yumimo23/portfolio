c4724092d917383317ede5b1b1aeab5aa16af4b5
yumimo23 <168384700+yumimo23@users.noreply.github.com>
2025-02-12 22:11:50 +0900

    header navi 書いた

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,12 +1,10 @@
 const nav = document.querySelector('.nav');
-
-// const headerBtn = document.getElementById('header__btn');
 const headerBtn = document.getElementById('menu');
 headerBtn.addEventListener('click', () => {
-    nav.classList.toggle('active');
+    var result = nav.classList.toggle('active');
 });
 
-const navBtns = document.querySelectorAll('.nav__btn, .nav__item a');
+const navBtns = document.querySelectorAll('.nav, .nav__itemSP a');
 navBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         nav.classList.remove('active');
@@ -15,18 +13,18 @@ navBtns.forEach(btn => {
 
 
 
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.getElementById('menu');
-    const navPage1 = document.querySelector('.nav-page1');
-    const navPage2 = document.querySelector('.nav-page2');
-    const menuLine1 = document.querySelector('.menu-line1');
-    const menuLine2 = document.querySelector('.menu-line2');
+// window.addEventListener('DOMContentLoaded', () => {
+//     const menu = document.getElementById('menu');
+//     const navPage1 = document.querySelector('.nav-page1');
+//     const navPage2 = document.querySelector('.nav-page2');
+//     const menuLine1 = document.querySelector('.menu-line1');
+//     const menuLine2 = document.querySelector('.menu-line2');
 
-    menu.addEventListener('click', () => {
-        menu.classList.toggle('rotate');
-        navPage1.classList.toggle('transform');
-        navPage2.classList.toggle('transform');
-        menuLine1.classList.toggle('rotate1');
-        menuLine2.classList.toggle('rotate2');
-    });
-});
+//     menu.addEventListener('click', () => {
+//         menu.classList.toggle('rotate');
+//         navPage1.classList.toggle('transform');
+//         navPage2.classList.toggle('transform');
+//         menuLine1.classList.toggle('rotate1');
+//         menuLine2.classList.toggle('rotate2');
+//     });
+// });
