0bac4443089eb50aaf751290c24df89f92df6c17
yumimo23 <168384700+yumimo23@users.noreply.github.com>
2025-02-12 20:44:07 +0900

    ヘッダーアニメーション途中

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector('.nav');
 
-const headerBtn = document.getElementById('header__btn');
+// const headerBtn = document.getElementById('header__btn');
+const headerBtn = document.getElementById('menu');
 headerBtn.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
@@ -9,5 +10,23 @@ const navBtns = document.querySelectorAll('.nav__btn, .nav__item a');
 navBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         nav.classList.remove('active');
+    });
+});
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.getElementById('menu');
+    const navPage1 = document.querySelector('.nav-page1');
+    const navPage2 = document.querySelector('.nav-page2');
+    const menuLine1 = document.querySelector('.menu-line1');
+    const menuLine2 = document.querySelector('.menu-line2');
+
+    menu.addEventListener('click', () => {
+        menu.classList.toggle('rotate');
+        navPage1.classList.toggle('transform');
+        navPage2.classList.toggle('transform');
+        menuLine1.classList.toggle('rotate1');
+        menuLine2.classList.toggle('rotate2');
     });
 });
