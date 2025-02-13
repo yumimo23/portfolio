828a43ef2d0c6f7eb09a6795576602463d9a66d9
yumimo23 <168384700+yumimo23@users.noreply.github.com>
2025-02-13 21:20:59 +0900

    footer upBtn

--- sample/menu02/menu02.js
+++ sample/menu02/menu02.js
@@ -1,14 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.getElementById('menu');
-    const navPage1 = document.querySelector('.nav-page1');
-    const navPage2 = document.querySelector('.nav-page2');
+    // const navPage1 = document.querySelector('.nav-page1');
+    // const navPage2 = document.querySelector('.nav-page2');
     const menuLine1 = document.querySelector('.menu-line1');
     const menuLine2 = document.querySelector('.menu-line2');
 
     menu.addEventListener('click', () => {
         menu.classList.toggle('rotate');
-        navPage1.classList.toggle('transform');
-        navPage2.classList.toggle('transform');
+        // navPage1.classList.toggle('transform');
+        // navPage2.classList.toggle('transform');
         menuLine1.classList.toggle('rotate1');
         menuLine2.classList.toggle('rotate2');
     });
