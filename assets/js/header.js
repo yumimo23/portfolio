2b1635ea7c2b5c33c720e65e6662f856344df9c0
yumimo23 <168384700+yumimo23@users.noreply.github.com>
2025-02-15 22:08:41 +0900

    JS整えた

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,26 +1,17 @@
 const nav = document.querySelector('.nav');
-const headerBtn = document.getElementById('menu');
-headerBtn.addEventListener('click', () => {
-    var result = nav.classList.toggle('active');
+const menu = document.getElementById('menu');
+const menuLine1 = document.querySelector('.menu-line1');
+const menuLine2 = document.querySelector('.menu-line2');
+menu.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    menu.classList.toggle('rotate');
+    menuLine1.classList.toggle('rotate1');
+    menuLine2.classList.toggle('rotate2');
 });
 
-// const navBtns = document.querySelectorAll('.nav, .nav__itemSP a');
-// navBtns.forEach(btn => {
-//     btn.addEventListener('click', () => {
-//         nav.classList.remove('active');
-//     });
-// });
-
-
-
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.getElementById('menu');
-    const menuLine1 = document.querySelector('.menu-line1');
-    const menuLine2 = document.querySelector('.menu-line2');
-
-    menu.addEventListener('click', () => {
-        menu.classList.toggle('rotate');
-        menuLine1.classList.toggle('rotate1');
-        menuLine2.classList.toggle('rotate2');
-    });
+nav.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    menu.classList.toggle('rotate');
+    menuLine1.classList.toggle('rotate1');
+    menuLine2.classList.toggle('rotate2');
 });
